2400f8f01825b85606c6decb86df0d60bdf2cfc2
HamidOmarr <you@example.com>
2023-04-10 16:02:54 +0200

    update2

--- Backend2/welkom.php
+++ Backend2/welkom.php
@@ -9,3 +9,19 @@
 			echo "<p>Geen gegevens ontvangen.</p>";
 		}
 	?>
+
+
+
+
+<!--POST-->
+<!-- ?php
+if (isset($_POST['naam']) && isset($_POST['email'])) {
+	$naam = $_POST['naam'];
+	$email = $_POST['email'];
+}
+?>
+
+	<h1>De ingevulde gegevens zijn:</h1>
+	<p>Naam: ?php echo $naam; ?></p>
+	<p>E-mailadres: ?php echo $email; ?></p> -->
+
